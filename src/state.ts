8945ee11e941c46632adecb9d822ff2142638edc
Razi Alsayyed <razi.sayed@gmail.com>
2024-11-04 22:27:33 +0200

    add state attributes

--- src/state.ts
+++ src/state.ts
@@ -4,10 +4,15 @@ export interface State {
     mseValidation: number | null;
     epoch: number | null;
     confusionMatrix: number[][] | null;
+    crossEntropyLoss: number | null;
+    predicted: number[] | null;
+    isLearning: boolean;
 }
 
 export function stateReducer(state: State, action: { type: string, payload?: any }): State {
-    if (action.type === 'SET_MSE') {
+    if (action.type === 'OVERRIDE') {
+        return action.payload;
+    } else if (action.type === 'SET_MSE') {
         return { ...state, mse: action.payload.mse };
     } else if (action.type === 'SET_MSE_VALIDATION') {
         return { ...state, mseValidation: action.payload.mseValidation };
@@ -15,6 +20,12 @@ export function stateReducer(state: State, action: { type: string, payload?: any
         return { ...state, epoch: action.payload.epoch };
     } else if (action.type === 'SET_CONFUSION_MATRIX') {
         return { ...state, confusionMatrix: action.payload.confusionMatrix };
+    } else if (action.type === 'SET_CROSS_ENTROPY_LOSS') {
+        return { ...state, crossEntropyLoss: action.payload.crossEntropyLoss };
+    } else if (action.type === 'SET_PREDICTED') {
+        return { ...state, predicted: action.payload.predicted };
+    } else if (action.type === 'SET_IS_LEARNING') {
+        return { ...state, isLearning: action.payload.isLearning };
     }
     throw Error('Unknown action.');
 }
