9d8d76d65d60576c4e75c714e63ad00ce9f2f312
Razi Alsayyed <razi.sayed@gmail.com>
2024-11-04 22:25:52 +0200

    move COLORS and SHAPES to constants.ts file

---
+++ src/constants.ts
@@ -0,0 +1,33 @@
+export const COLORS = [
+    "fill-red-300",
+    "fill-lime-300",
+    "fill-cyan-300",
+    "fill-violet-300",
+
+    "fill-orange-300",
+    "fill-emerald-300",
+    "fill-sky-300",
+    "fill-blue-300",
+
+    "fill-yellow-300",
+    "fill-teal-300",
+    "fill-fuchsia-300",
+    "fill-purple-300",
+];
+
+export const SHAPES = [
+    "circle",
+    "rect",
+    "triangle",
+    "45degrect",
+
+    "circle",
+    "45degrect",
+    "rect",
+    "circle",
+
+    "triangle",
+    "rect",
+    "triangle",
+    "45degrect",
+]
